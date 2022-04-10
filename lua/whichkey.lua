ebbc13b6f76af3a85d038f05e296f57aa675da6d
mengqinyu.01 <mengqinyu.01@bytedance.com>
2022-04-10 18:16:48 +0800

    fix: fix some keymap
    Change-Id: I28e7a0baf33fe0a7fa05a36e821430aacfed0868

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -111,7 +111,7 @@ local mappings = {
   --   "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
   --   "Find files",
   -- },
-  ["F"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+  ["f"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
   ["p"] = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
 
   -- p = {
@@ -194,16 +194,7 @@ local mappings = {
     C = { "<cmd>Telescope commands<cr>", "Commands" },
   },
 
-  t = {
-    name = "Terminal",
-    -- n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" },
-    -- u = { "<cmd>lua _NCDU_TOGGLE()<cr>", "NCDU" },
-    -- t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" },
-    -- p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" },
-    f = { "<cmd>ToggleTerm direction=float<cr>", "Float" },
-    h = { "<cmd>ToggleTerm size=10 direction=horizontal<cr>", "Horizontal" },
-    v = { "<cmd>ToggleTerm size=80 direction=vertical<cr>", "Vertical" },
-  },
+  -- ["t"] = {"<cmd>lua require('FTerm').toggle()<cr>", "Float Term"}
 }
 
 local pure_mappings = {
